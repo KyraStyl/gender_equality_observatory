00d46af6423b4c3d260fe60c1a60e46688c9ddad
Tourgito <dai16057@uom.edu.gr>
2021-06-04 18:20:15 +0300

    Cypher queries that return information about gender inequality

--- webapp/genderCharacteristicsQueries.py
+++ webapp/genderCharacteristicsQueries.py
@@ -1,5 +1,119 @@
 from neo4j import GraphDatabase
-from .models import Professor,driver
+from models import Professor,driver
+
+
+
+# Return the average number of publications of a female and a male professor
+def getAverageNumberOfPublicationsOfMaleAndFemaleProfessor()-> list:
+
+    """
+    ######## Returns ##########################
+    [['M', 184.63986013986016], ['F', 127.72549019607841]]
+    """
+
+    with driver.session() as session:
+        avgNumberOfPublications = session.run("MATCH (p:Professor) \
+                                            WHERE EXISTS(p.num_publications) \
+                                            RETURN p.gender, avg(p.num_publications) "\
+                                            )
+        return avgNumberOfPublications.values()
+
+
+
+# Return the average number of coauthors of a female and a male professor
+def getAverageNumberOfCoauthorsOfMaleAndFemaleProfessor()-> dict:
+
+    """
+    ######## Returns ##########################
+    {'M': 12.74891774891775, 'F': 9.261904761904763}
+    """
+
+    with driver.session() as session:
+        avgNumberOfCoauthors = session.run("MATCH (p:Professor)-[r:cooperateWith]-(p2) \
+                                            RETURN p.name, p.gender, collect(p2.name) "\
+                                            )
+        numberOfMales = 0 
+        numberOfFemales = 0
+        numberOfMalesCoauthors = 0
+        numberOfFemalesCoauthors = 0
+        for index, professor in enumerate(avgNumberOfCoauthors.values()):
+            if professor[1] == "M":
+                numberOfMales += 1
+                numberOfMalesCoauthors += len(professor[2])
+            else:
+                numberOfFemales += 1
+                numberOfFemalesCoauthors += len(professor[2])
+            
+        return {"M":numberOfMalesCoauthors/numberOfMales,"F":numberOfFemalesCoauthors/numberOfFemales}
+
+"""
+# Return the average number of publications of a female and a male professor
+def getNumberOfMalesAndFemalesPerRole(driver)-> list:
+
+
+    roleGenderDistribution = dict()
+    with driver.session() as session:
+        numberOfProfessoresPerRole = session.run("MATCH (p:Professor) \
+                                            RETURN p.gender, p.role, count(p) "\
+                                            )
+        for role in numberOfProfessoresPerRole.values():
+            if str(role[0]) != "nar" and type(role[0]) != float :
+                print(role[0])
+                if role[0][-1] == " ":
+                    role[0] = role[0][:-1]  
+                if role[0] not in roleGenderDistribution.keys(): 
+                    roleGenderDistribution[role[0]] = {role[1]:role[2]}
+                else:
+                   a = roleGenderDistribution[role[0]] 
+                   a[role[1]] = role[2]  
+                   roleGenderDistribution[role[0]] = a
+        #return roleGenderDistribution
+        return numberOfProfessoresPerRole.values()
+"""
+
+# Return the average number of citations of a female and a male professor
+def getAverageNumberOfCitationsOfMaleAndFemaleProfessor()-> list:
+
+    """
+    ######## Returns ##########################
+    [['M', 3662.1503496503533], ['F', 2203.5294117647054]]
+    """
+
+    with driver.session() as session:
+        avgNumberOfCitations = session.run("MATCH (p:Professor) \
+                                            RETURN p.gender, avg(p.citedby) "\
+                                            )
+        return avgNumberOfCitations.values()
+
+# Return the average number of h-index of a female and a male professor
+def getAverageNumberOfHIndexOfMaleAndFemaleProfessor()-> list:
+
+    """
+    ######## Returns ##########################
+    [['M', 23.307692307692292], ['F', 18.823529411764714]]
+    """
+
+    with driver.session() as session:
+        avgNumberOfHIndex = session.run("MATCH (p:Professor) \
+                                            RETURN p.gender, avg(p.hindex) "\
+                                            )
+        return avgNumberOfHIndex.values()
+
+# Return the average number of i10-index of a female and a male professor
+def getAverageNumberOfI10IndexOfMaleAndFemaleProfessor()-> list:
+
+    """
+    ######## Returns ##########################
+    [['M', 55.583916083916066], ['F', 36.98039215686275]]
+    """
+
+    with driver.session() as session:
+        avgNumberOfI10Index = session.run("MATCH (p:Professor) \
+                                            RETURN p.gender, avg(p.i10index) "\
+                                            )
+        return avgNumberOfI10Index.values()
+
+
 
 # Return the K professors that have the most coauthors
 def getTopKProfessorsWithMostCoauthors(numberOfProfessorsToReturn:int)-> list:
@@ -18,6 +132,7 @@ def getTopKProfessorsWithMostCoauthors(numberOfProfessorsToReturn:int)-> list:
         return coauthors.values()
 
 
+
 # Returns the K professor with highest page rank score in the network in descending order
 def topKProfessorsWithHighestPageRankScore(topK:int)-> list:
 
@@ -25,7 +140,7 @@ def topKProfessorsWithHighestPageRankScore(topK:int)-> list:
     ########### Return for topk=3 ###############
     [['Nicolas PERRY', 'M', 6.9452032727334245], ['Georg Schmitz', 'M', 6.273291582755126], ['Peter Mark', 'M', 6.123292444521661]]
     """
-
+    
     with driver.session() as session:
         topKProfessors = session.run("CALL gds.pageRank.stream('my-graph') \
                                     YIELD nodeId, score \
@@ -35,6 +150,26 @@ def topKProfessorsWithHighestPageRankScore(topK:int)-> list:
                                     LIMIT $topK" \
                                     ,topK=topK)
         return topKProfessors.values()
+
+
+# Returns the average page rank score for Female and male professors
+def avgPageRankScoreOfFemaleAndMaleProfessor()-> list:
+
+    """
+    ########### Return ###############
+    [['M', 2.0929593585778217], ['F', 1.7305735249165284]]
+    """
+    
+    with driver.session() as session:
+        avgPageRankScore = session.run("CALL gds.pageRank.stream('my-graph') \
+                                    YIELD nodeId, score \
+                                    WHERE labels(gds.util.asNode(nodeId)) = ['Professor'] \
+                                    RETURN gds.util.asNode(nodeId).gender, avg(score)" \
+                                    )
+
+        return avgPageRankScore.values()
+
+
 
 
 # Returns the K professor with highest betweenes score in the network in descending order
@@ -54,6 +189,23 @@ def topKProfessorsWithHighestBetweenes(topK:int)-> list:
                                     LIMIT $topK" \
                                     ,topK=topK)
         return topKProfessors.values()
+
+# Returns the average betweenes score for Female and male professors
+def avgBetweenesScoreOfFemaleAndMaleProfessor()-> list:
+
+    """
+    ########### Return ###############
+    [['M', 30604.032270899068], ['F', 17960.359620130537]]
+    """
+    
+    with driver.session() as session:
+        avgBetweenessScore = session.run("CALL gds.betweenness.stream('my-graph') \
+                                    YIELD nodeId, score \
+                                    WHERE labels(gds.util.asNode(nodeId)) = ['Professor'] \
+                                    RETURN gds.util.asNode(nodeId).gender, avg(score)" \
+                                    )
+
+        return avgBetweenessScore.values()
 
 
 # Returns the K professor with highest degree centrality score in the network in descending order
@@ -75,6 +227,26 @@ def topKProfessorsWithHighestDegreeCentrality(topK:int)-> list:
         return topKProfessors.values()
 
 
+# Returns the average degree centrality score for Female and male professors
+def avgDegreeCentralityScoreOfFemaleAndMaleProfessor()-> list:
+
+    """
+    ########### Return ###############
+    [['M', 20.59440559440562], ['F', 15.254901960784311]]
+    """
+    
+    with driver.session() as session:
+        avgDegreeCentralityScore = session.run("CALL gds.degree.stream('my-graph',{ orientation: 'UNDIRECTED' }) \
+                                    YIELD nodeId, score \
+                                    WHERE labels(gds.util.asNode(nodeId)) = ['Professor'] \
+                                    RETURN gds.util.asNode(nodeId).gender, avg(score)" \
+                                    )
+
+        return avgDegreeCentralityScore.values()
+
+
+
+
 # Returns the K professor with highest closeness centrality score in the network in descending order
 def topKProfessorsWithHighestClosenessCentrality(topK:int)-> list:
 
@@ -93,6 +265,27 @@ def topKProfessorsWithHighestClosenessCentrality(topK:int)-> list:
                                     ,topK=topK)
         return topKProfessors.values()
 
+
+# Returns average closeness centrality score for a Male and a Female Professor 
+def avgClosenessCentralityOfFemaleAndMaleProfessor()-> list:
+
+    """
+    ########### Return for topk=3 ###############
+    [['M', 0.14054242305828504], ['F', 0.14950386305069283]]
+    """
+
+    with driver.session() as session:
+        avgClosenessCentrality = session.run("CALL gds.alpha.closeness.stream('my-graph') \
+                                        YIELD nodeId, centrality \
+                                        WHERE labels(gds.util.asNode(nodeId)) = ['Professor'] \
+                                        RETURN gds.util.asNode(nodeId).gender, avg(centrality)" \
+                                        )
+        return avgClosenessCentrality.values()
+
+
+
+
+
 # Returns the K professor with highest closeness harmonic centrality score in the network in descending order
 def topKProfessorsWithHighestClosenessHarmonicCentrality(topK:int)-> list:
 
@@ -110,6 +303,27 @@ def topKProfessorsWithHighestClosenessHarmonicCentrality(topK:int)-> list:
                                     LIMIT $topK" \
                                     ,topK=topK)
         return topKProfessors.values()
+
+
+# Returns average closeness harmonic centrality score for a Male and a Female Professor 
+def avgClosenessHarmonicCentralityOfFemaleAndMaleProfessor()-> list:
+
+    """
+    ########### Returns ###############
+    [['M', 0.10544925801175273], ['F', 0.10012851638528246]]
+    """
+
+    with driver.session() as session:
+        avgClosenessCentrality = session.run("CALL gds.alpha.closeness.harmonic.stream('my-graph') \
+                                        YIELD nodeId, centrality \
+                                        WHERE labels(gds.util.asNode(nodeId)) = ['Professor'] \
+                                        RETURN gds.util.asNode(nodeId).gender, avg(centrality)" \
+                                        )
+        return avgClosenessCentrality.values()
+
+
+
+
 
 
 # Returns the K professor with highest spread information influenc score in the network in descending order
